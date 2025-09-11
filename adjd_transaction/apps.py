03937e63859563a7a53e22bfb2def3aa4f62a018
Mohamedayman-1 <mayman@lightidea.org>
2025-09-11 10:59:09 +0300

    .

--- adjd_transaction/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class AdjdTransactionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adjd_transaction'
-    verbose_name = 'ADJD Transaction Management'
-    def ready(self):
-        import adjd_transaction.signals
